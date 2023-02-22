2ca78bab886e36593d4b680465f3f9243d5cda4b
misternooblet <dragonroadsupport@gmail.com>
2023-02-22 20:29:23 +0200

    18.1

---
+++ 18.1/src/App.js
@@ -0,0 +1,34 @@
+
+import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./components/Header";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+import Register from "./Pages/Register";
+
+import Admin from "./Pages/Admin";
+import NoPermissions from "./Pages/NoPermissions";
+
+
+
+
+function App() {
+  const [isLogged, setIsLogged] = useState(false)
+  const [isAdmin, setIsAdmin] = useState(false)
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Header setIsLogged={setIsLogged} />,
+      children: [
+        { path: '/', element: isLogged ? <Home user={isLogged} /> : <Login isLogged={setIsLogged} setIsAdmin={setIsAdmin} /> },
+        { path: '/register', element: <Register /> },
+        { path: '/admin', element: isAdmin ? <Admin /> : <NoPermissions /> }
+      ]
+    }
+  ])
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
